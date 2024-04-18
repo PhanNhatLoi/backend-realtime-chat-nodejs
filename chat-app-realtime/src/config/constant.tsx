a90fe8e94639cea581093338b9d34675fe527a58
PhanNhatLoi <phanloi971@gmail.com>
2024-04-18 23:53:59 +0700

    Update constant.tsx

--- chat-app-realtime/src/config/constant.tsx
+++ chat-app-realtime/src/config/constant.tsx
@@ -1,3 +1,4 @@
-export const PORT = 3000;
-
-export const SERVER_URL = "http://localhost:8000";
+export const SERVER_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://server-nodejs-iota.vercel.app"
+    : "http://localhost:8000";
